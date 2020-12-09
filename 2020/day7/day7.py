1a685679387f2274cae38b6364f676f0857eafcd
Dan Perret <dperret@mitre.org>
2020-12-08 23:49:24 -0500

    Adding day 7 and day 8.

---
+++ 2020/day7/day7.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+
+class Bag:
+    def __init__(self):
+        self.contents = []
+        self.outercolor = None
+
+    def __str__(self):
+        return "{} bag contents: {}".format(self.outercolor, self.contents)
+
+class Part2Bag:
+    def __init__(self):
+        self.contents = []
+        self.outercolor = None
+
+    def __str__(self):
+        return "{} bag contents: {}".format(self.outercolor, self.contents)
+
+
+def findPossibleOuterBags(allBags, targetBag):
+    possibleBags = []
+    for bag in allBags:
+        if(targetBag in bag.contents):
+            #print(bag)
+            #print(bag.outercolor)
+            if(bag.outercolor not in possibleBags):
+                possibleBags.append(bag.outercolor)
+            nextlevel = findPossibleOuterBags(allBags, bag.outercolor)
+            for item in nextlevel:
+                if(item not in possibleBags):
+                    possibleBags.append(item)
+    return possibleBags
+
+def findTotalBags(allBags, targetBag):
+    # Include targetBag as 1 bag
+    total = 1
+    objectBag = None
+    for bag in allBags:
+        if(bag.outercolor == targetBag):
+            objectBag = bag
+            break
+    if(objectBag != None):
+        for innerbag in objectBag.contents:
+            total += findTotalBags(allBags, innerbag)
+    return total
+
+def part1(lines):
+    possibleBags = []
+    for line in lines:
+        if(len(line) < 1):
+            continue
+        index = line.find(" bags contain ")
+        outerbag = line[:index]
+        #print("Outer bag: {}".format(outerbag))
+        newBag = Bag()
+        newBag.outercolor = outerbag
+        secondhalf = line[index + 14:]
+        if(secondhalf == "no other bags."):
+            continue
+        else:
+            parts = secondhalf.split(',')
+            #print(parts)
+            for part in parts:
+                item = part.strip()
+                quantity = item[0]
+                bagindex = item.find(' bag')
+                innerbag = item[2:bagindex]
+                #print("{}: {}".format(quantity, innerbag))
+                #for i in range(0,int(quantity)):
+                #    newBag.contents.append(innerbag)
+                newBag.contents.append(innerbag)
+                #print(newBag)
+        possibleBags.append(newBag)
+    outerbags = findPossibleOuterBags(possibleBags, 'shiny gold')
+    return len(outerbags)
+
+def part2(lines):
+    possibleBags = []
+    for line in lines:
+        if(len(line) < 1):
+            continue
+        index = line.find(" bags contain ")
+        outerbag = line[:index]
+        #print("Outer bag: {}".format(outerbag))
+        newBag = Bag()
+        newBag.outercolor = outerbag
+        secondhalf = line[index + 14:]
+        if(secondhalf == "no other bags."):
+            continue
+        else:
+            parts = secondhalf.split(',')
+            #print(parts)
+            for part in parts:
+                item = part.strip()
+                quantity = item[0]
+                bagindex = item.find(' bag')
+                innerbag = item[2:bagindex]
+                #print("{}: {}".format(quantity, innerbag))
+                for i in range(0,int(quantity)):
+                    newBag.contents.append(innerbag)
+                #newBag.contents.append(innerbag)
+                #print(newBag)
+        possibleBags.append(newBag)
+    # TODO: Figure out why findTotalBags is off by 1
+    totalbags = findTotalBags(possibleBags, 'shiny gold') - 1
+    return totalbags
+
+
+with open("input", "r") as fh:
+    lines = fh.read().split("\n")
+    print("Part 1: {}".format(part1(lines)))
+    print("Part 2: {}".format(part2(lines)))
---
+++ 2020/day8/day8.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+import statistics
+
+
+def processInstruction(index, accumulator, line, modifyInstruction):
+    if(line == ""):
+        return (index, accumulator)
+    parts = line.split(' ')
+    instruction = parts[0]
+    value = int(parts[1])
+    if(index == modifyInstruction):
+        if(instruction == "jmp"):
+            instruction = "nop"
+        elif (instruction == "nop"):
+            instruction = "jmp"
+    if(instruction == "acc"):
+        return (index + 1, accumulator + value)
+    elif(instruction == "jmp"):
+        return (index + value, accumulator)
+    elif(instruction == "nop"):
+        return (index + 1, accumulator)
+
+def part1(lines):
+    i = 0
+    accumulator = 0
+    linesExecuted = []
+    while i < len(lines):
+        if(i in linesExecuted):
+            print("Duplicate instruction: {}".format(i))
+            return accumulator
+        linesExecuted.append(i)
+        i, accumulator = processInstruction(i, accumulator, lines[i], -1)
+    return accumulator
+
+def part2(lines, modifyInstruction):
+    i = 0
+    accumulator = 0
+    linesExecuted = []
+    counter = 0
+    duplicateJumpsExecuted = []
+    while i < len(lines):
+        if(counter > 10000):
+            return -1
+        #if(i in linesExecuted):
+        #    # print("Duplicate instruction: {}: {}".format(i, lines[i]))
+        #    if(lines[i][:5] == "jmp -"):
+        #        duplicateJumpsExecuted.append(i)
+        #        if(len(duplicateJumpsExecuted) > 10000):
+        #            #for item in duplicateJumpsExecuted:
+        #            #    print(item)
+        #            #print(duplicateJumpsExecuted)
+        #            #print(statistics.mode(duplicateJumpsExecuted))
+        #            return accumulator
+        linesExecuted.append(i)
+        counter += 1
+        currentInstruction = i
+        i, accumulator = processInstruction(i, accumulator, lines[i], modifyInstruction)
+        if(i == currentInstruction):
+            break
+    print("Finished! i: {} accumulator: {}".format(i, accumulator))
+    return accumulator
+
+def identifyCandidates(lines):
+    candidates = []
+    for i in range(1,len(lines)-1):
+        instruction = lines[i][:3]
+        previousInstruction = lines[i-1][:3]
+        nextInstruction = lines[i+1][:3]
+        if instruction == "acc":
+            continue
+        elif instruction == "jmp":
+            candidates.append(i)
+            #if nextInstruction == "jmp":
+            #    continue
+            #else:
+            #    candidates.append(i)
+        elif instruction == "nop":
+            candidates.append(i)
+            #if int(lines[i][4:]) >= 0:
+            #    candidates.append(i)
+    #for item in changeInstructions:
+    #    candidates.remove(item)
+    return candidates
+
+
+with open("input", "r") as fh:
+    lines = fh.read().split("\n")
+    changeInstructions = identifyCandidates(lines)
+    #changeInstructions = range(0,623)
+    #changeInstructions = [298]
+    for modifyInstruction in changeInstructions:
+        accumulator = part2(lines, modifyInstruction)
+        if(accumulator != -1):
+            print("modifyInstruction: {}".format(modifyInstruction))
+            print("Part 2: {}".format(accumulator))
+            exit()
